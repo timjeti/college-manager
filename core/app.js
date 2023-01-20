e679b3970fddb71a871d29e2f30b18fd7f979838
timjeti <jtimsina@gmail.com>
2023-01-20 09:21:49 +0530

    Db connectivity established

--- core/app.js
+++ core/app.js
@@ -1,10 +1,30 @@
 const express = require('express')
+const mysql = require('mysql')
 const app = express()
 const port = 3001
 
-app.get('/',(req,res) => {
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'Metamorphosis1992@',
+	database: 'college'
+  })
 
-	res.send('Hello Jeevan!!')
+connection.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected!");
+  });
+
+
+app.get('/',(req,res) => {
+	data = {}
+	connection.query('SELECT * FROM test', (err, rows, fields) => {
+		console.log('Inside db api')
+		console.log('The solution is: ', rows)
+		data = rows
+	  })
+	console.log('Inside get api')
+	res.send(data)
 })
 
 app.listen(port, () => {
